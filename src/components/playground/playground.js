7caa8201ae193eb51bfdefe572bdcc1216562d1b
Kurara Sansara <kurara.onion@gmail.com>
2020-11-16 15:36:47 +0300

    update playground.js

--- src/components/playground/playground.js
+++ src/components/playground/playground.js
@@ -1,9 +1,11 @@
-function fillTable(lLength, cLength, table) {
+function fillTable(lLength, cLength) {
+  let table = document.createElement('table')
   for (let i = 0; i < lLength; i++) {
     let line = document.createElement('tr')
     for (let j = 0; j < cLength; j++) {
       let cell = document.createElement('td')
       cell.innerText = ((cLength) * i) + j + 1
+      cell.setAttribute('id', cell.innerText)
       line.append(cell)
     }
     table.append(line)
@@ -14,27 +16,28 @@ function fillTable(lLength, cLength, table) {
 
 function Playground(props){
 
-  let table = document.createElement('table')
+  let table = null;
 
   switch (props.difficult) {
     case '20':
-      table = fillTable(4, 5, table)
+      table = fillTable(4, 5)
       break;
     case '24':
-      table = fillTable(6, 4, table)
+      table = fillTable(6, 4)
       break;
     case '30':
-      table = fillTable(6, 5, table)
+      table = fillTable(6, 5)
       break;
   }
 
-  console.log(table);
-
- document.body.append(table)
+  setTimeout(() => {
+    let div = document.getElementById('play-table')
+    div.insertAdjacentHTML('afterbegin', table.outerHTML)
+  }, 0)
 
   return (
-    <div>
-      {Object.toString(table)}
+    <div id = 'play-table'>
+
     </div>
   )
 }
