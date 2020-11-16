7997daccc36403145de682f04d8e6c2383dca03f
Kurara Sansara <kurara.onion@gmail.com>
2020-11-16 14:10:15 +0300

    Basic structure

---
+++ src/components/main-menu/main-menu.js
@@ -0,0 +1,16 @@
+import './main-menu.css'
+
+function MainMenu(){
+  return (
+    <div className = 'menu-buttons'>
+      <div id = 'button'>
+        <button>new game</button>
+      </div>
+      <div id = 'button'>
+        <button>leaderboard</button>
+      </div>
+    </div>
+  )
+}
+
+export default MainMenu;
