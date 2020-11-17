04d6427c69000141cff51e766f9959134519a87c
kurara sansara <kurara.onion@gmail.com>
2020-11-17 18:41:45 +0300

    fixed game mechanic

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,16 +1,44 @@
 import './card.css'
 import React from 'react'
 
+localStorage.clear()
+
 function Card(props) {
   function handleClick(e) {
-    console.log(e.target);
     let elem = e.target.closest('td').firstElementChild.firstElementChild
-    elem.classList.toggle('hide')
+    elem.classList.add('open')
+
+
+
+
+    let id = props.card.value
+
+    if(localStorage.length === 0) {
+      localStorage.setItem(id, props.card.url)
+      elem.classList.add('clicked')
+
+    } else if(localStorage.getItem(id)) {
+      alert('ura pobeda');
+      localStorage.clear()
+
+      let cards = document.querySelectorAll(`[value="${id}"]`)
+      cards.forEach((item) => {
+        item.classList.add('hide')
+      });
+
+
+
+    } else {
+      localStorage.clear()
+    }
+
+
+
 
 
   }
   return (
-		<div className = 'card-holder' onClick = {handleClick}>
+		<div value = {props.card.value} className = 'card-holder' onClick = {handleClick}>
       <img src = {props.card.url} className = 'front'/>
       <img src = '/png/back/back@2x.png' className = 'back' />
 		</div>
